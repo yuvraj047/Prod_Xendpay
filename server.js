89ea0f7b176a4478f5110f21a5d8a2a8468191a6
Yuvraj Srivastava <yuvrajsrivastav242002@gmail.com>
2026-08-29 16:49:28 +0530

    Change route to capture all paths with splat - bug solve

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const distPath = path.join(__dirname, "dist");
 
 app.use(express.static(distPath));
 
-app.get("*", (req, res) => {
+app.get("/{*splat}", (req, res) => {
   res.sendFile(path.join(distPath, "index.html"));
 });
 
